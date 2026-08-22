79d2ec3184493fb424aa7d52dd8dfc6dfc0a115c
Karishma Tank <tankkarishma@gmail.com>
2026-08-21 21:00:39 -0400

    Adds vite config file and fixes persistence worker env variable

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        invalid: resolve(__dirname, 'invalid.html'),
+      },
+    },
+  },
+});
